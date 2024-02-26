35b0abd36108a17ca0942ced8810681ad1889234
dupeng1 <1115378691@qq.com>
2024-02-26 08:29:50 +0800

    springwebmvc注释

--- spring-webmvc/src/main/java/org/springframework/web/servlet/HandlerAdapter.java
+++ spring-webmvc/src/main/java/org/springframework/web/servlet/HandlerAdapter.java
@@ -47,6 +47,10 @@ import org.springframework.lang.Nullable;
  * @see org.springframework.web.servlet.mvc.SimpleControllerHandlerAdapter
  * @see org.springframework.web.servlet.handler.SimpleServletHandlerAdapter
  */
+
+/**
+ * https://blog.csdn.net/Coder_Boy_/article/details/129187882?ops_request_misc=%257B%2522request%255Fid%2522%253A%2522170887508616800225595004%2522%252C%2522scm%2522%253A%252220140713.130102334.pc%255Fblog.%2522%257D&request_id=170887508616800225595004&biz_id=0&utm_medium=distribute.pc_search_result.none-task-blog-2~blog~first_rank_ecpm_v1~rank_v31_ecpm-13-129187882-null-null.nonecase&utm_term=spring%20mvc&spm=1018.2226.3001.4450
+ */
 public interface HandlerAdapter {
 
 	/**
